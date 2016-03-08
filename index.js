a65d7b4fe994493a7a34ff5a4dc0a2ebf5e86892
Jonas Keinholz <jonas@keinholz.com>
2016-03-08 15:38:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  },
+  'rules': {
+    'jsx-quotes': 2,
+    'react/display-name': [2, { 'acceptTranspilerName': true }],
+    'react/jsx-boolean-value': [2, 'never'],
+    'react/jsx-closing-bracket-location': [2, 'after-props'],
+    'react/jsx-curly-spacing': 2,
+    'react/jsx-equals-spacing': 2,
+    'react/jsx-indent': [2, 2],
+    'react/jsx-key': 2,
+    'react/jsx-no-bind': 2,
+    'react/jsx-no-duplicate-props': 2,
+    'react/jsx-no-undef': 2,
+    'react/jsx-pascal-case': 2,
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/no-deprecated': 2,
+    'react/no-did-mount-set-state': 2,
+    'react/no-did-update-set-state': 2,
+    'react/no-direct-mutation-state': 2,
+    'react/no-is-mounted': 2,
+    'react/no-unknown-property': 2,
+    'react/prefer-es6-class': 2,
+    'react/prop-types': 2,
+    'react/react-in-jsx-scope': 2,
+    'react/require-extension': 2,
+    'react/self-closing-comp': 2,
+    'react/sort-comp': 2,
+    'react/wrap-multilines': 2
+  },
+  'plugins': [
+    'react'
+  ]
+}
